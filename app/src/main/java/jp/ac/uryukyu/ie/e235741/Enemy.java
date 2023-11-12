22320eabd8027fe6f1d81fb6ffc99e6f8926a97f
Yuito Arakaki <e235741@ie.u-ryukyu.ac.jp>
2023-11-12 20:00:22 +0900

    3rd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235741/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235741/Enemy.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e235741;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -38,7 +38,7 @@ public class Enemy {
     public void attack(Hero hero) {
         if (this.hitPoint > 0) {
             int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
         }
     }
@@ -57,4 +57,35 @@ public class Enemy {
         }
     }
 
+public String getName(){
+        return name;
+    } 
+
+    public int getHitPoint(){
+        return hitPoint;
+    }
+
+    public int getAttack(){
+        return attack;
+    }
+
+    public boolean getDead(){
+        return true;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    public void setDead(boolean bool){
+        this.dead = bool;
+    }
 }
